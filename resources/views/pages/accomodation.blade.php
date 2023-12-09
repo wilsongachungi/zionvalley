cd68e1c031f019303cf310b5ced8ae87e6403fd6
robmainah <robmainah@gmail.com>
2023-12-09 23:48:00 +0300

    refactor accomodation page

--- resources/views/pages/accomodation.blade.php
+++ resources/views/pages/accomodation.blade.php
@@ -1,111 +1,108 @@
 <x-base-layout>
-    <div class="page-hero bg-image "
-        style="background-image: url(../assets/img/banneraccommodation.jpg); opacity: 0.5;width: 100%; height:400px">
-        <div class="hero-section">
-            <div class="container text-center wow zoomIn">
-                <span class="subhead">Coming soon..!!</span>
-                <h5 class="display-4">Accommodation</h5>
-            </div>
-        </div>
-    </div>
+	<div class="accomodation">
+		<div class="page-hero bg-image">
+			<div class="hero-section">
+				<div class="container text-center wow zoomIn">
+					<h1 class="display-4">Accommodation</h1>
+					<span class="subhead">Coming soon..!!</span>
+				</div>
+			</div>
+		</div>
+		<div class="container">
+			<section id="accommodation" class="content">
+				<div class="row">
+					<div class="col-12 col-md-4 single-accommodation">
+						<img src="../assets/img/coolplace.jpg" alt="">
+						<div class="overlay"></div>
+						<div class="service-desc pl-2 pb-2">Tranquil view</div>
+					</div>
+					<div class="col-12 col-md-4 single-accommodation">
+						<img src="../assets/img/23 slope.jpg" alt="">
+						<div class="overlay"></div>
+						<div class="service-desc pl-2 pb-2">Cool air Breeze</div>
+					</div>
+					<div class="col-12 col-md-4 single-accommodation">
+						<img src="../assets/img/25 slope.jpg" alt="">
+						<div class="overlay"></div>
+						<div class="service-desc pl-2 pb-2">Cottages of your choice</div>
+					</div>
+					<div class="col-12 col-md-4 single-accommodation">
+						<img src="../assets/img/coolplace2.jpg" alt="">
+						<div class="overlay"></div>
+						<div class="service-desc pl-2 pb-2">Cool place and private for nights</div>
+					</div>
+					<div class="col-12 col-md-4 single-accommodation">
+						<img src="../assets/img/13 slope.jpg" alt="">
+						<div class="overlay"></div>
+						<div class="service-desc pl-2 pb-2">Nature experience</div>
+					</div>
+					<div class="col-12 col-md-4 single-accommodation">
+						<img src="../assets/img/10 slope.jpg" alt="">
+						<div class="overlay"></div>
+						<div class="service-desc pl-2 pb-2">>River and Waterfall.</p>
+						</div>
+					</div>
+				</div>
+			</section>
+			<div class="text-justify">
+				<h2 class="text-center mb-4">Free Accommodation to Harambee owner</h2>
+				<p class=" line-height-2">
+					Lorem ipsum dolor sit amet consectetur adipisicing elit. Omnis beatae non eligendi
+					possimus maxime saepe expedita quam eius. Quas, nihil quo assumenda quod quibusdam
+					nobis maiores dignissimos obcaecati ex autem veritatis architecto aliquam accusamus
+					blanditiis voluptate rem distinctio necessitatibus vero alias! Nemo labore repellat
+					quam iure numquam voluptatum necessitatibus quia.
+				</p>
+				<p class="text-justify line-height-2">
+					A menber to own One romantic cottage one weekend each and every year - two nights per year - For
+					every 25k Tree Certificates Held
+				</p>
+			</div>
 
-    <section id="accommodation">
-        <div class="accommodation-box">
-            <div class="single-accommodation">
-                <img src="../assets/img/coolplace.jpg" alt="">
-                <div class="overlay"></div>
-                <div class="service-desc">
-                    <p>Tranquil view</p>
-                </div>
-            </div>
-            <div class="single-accommodation">
-                <img src="../assets/img/23 slope.jpg" alt="">
-                <div class="overlay"></div>
-                <div class="service-desc">
-
-                    <p>Cool air Breez</p>
-                </div>
-            </div>
-            <div class="single-accommodation">
-                <img src="../assets/img/25 slope.jpg" alt="">
-                <div class="overlay"></div>
-                <div class="service-desc">
-                    <p>Cottages of your choice</p>
-                </div>
-            </div>
-            <div class="single-accommodation">
-                <img src="../assets/img/coolplace2.jpg" alt="">
-                <div class="overlay"></div>
-                <div class="service-desc">
-
-                    <p>Cool place and private for nights</p>
-                </div>
-            </div>
-            <div class="single-accommodation">
-                <img src="../assets/img/13 slope.jpg" alt="">
-                <div class="overlay"></div>
-                <div class="service-desc">
-
-                    <p>Nature experience</p>
-                </div>
-            </div>
-            <div class="single-accommodation">
-                <img src="../assets/img/10 slope.jpg" alt="">
-                <div class="overlay"></div>
-                <div class="service-desc">
-                    <p>River and Waterfall view
-                        .</p>
-                </div>
-            </div>
-        </div>
-
-    </section>
-
-    <div class="container">
-        <div class="row">
-            <h4 style="color:greenyellow">Free Accommodation to Harambee owner</h4>
-            <div class="col-sm-8">
-                <p style="text-end">Lorem ipsum dolor sit amet consectetur adipisicing elit. Omnis beatae non eligendi
-                    possimus maxime saepe expedita quam eius. Quas, nihil quo assumenda quod quibusdam
-                    nobis maiores dignissimos obcaecati ex autem veritatis architecto aliquam accusamus
-                    blanditiis voluptate rem distinctio necessitatibus vero alias! Nemo labore repellat
-                    quam iure numquam voluptatum necessitatibus quia.</p>
-            </div>
-            <div class="col-sm-4">
-                <p>a menber to own One romantic cottage one weekend each and every year - two nights
-                    per year - For every 25k Tree Certificates Held</p>
-            </div>
-        </div>
-    </div>
-
-    <div class="container text-center position-relative">
-        <div id="imageCarousel" class="carousel slide" data-bs-ride="carousel">
-            <div class="carousel-inner">
-                <div class="carousel-item active">
-                    <div class="d-flex justify-content-center">
-                        <img src="../assets/img/10 slope.jpg" class="w-25" alt="Image 1">
-                        <img src="../assets/img/10 slope.jpg" class="w-25" alt="Image 2">
-                        <img src="../assets/img/10 slope.jpg" class="w-25" alt="Image 3">
-                    </div>
-                </div>
-                <div class="carousel-item">
-                    <div class="d-flex justify-content-center">
-                        <img src="image4.jpg" class="w-25" alt="Image 4">
-                        <img src="image5.jpg" class="w-25" alt="Image 5">
-                        <img src="image6.jpg" class="w-25" alt="Image 6">
-                    </div>
-                </div>
-
-            </div>
-
-            <button class="custom-carousel-control custom-carousel-prev" type="button" data-bs-target="#imageCarousel"
-                data-bs-slide="prev">
-                &lt;
-            </button>
-            <button class="custom-carousel-control custom-carousel-next" type="button" data-bs-target="#imageCarousel"
-                data-bs-slide="next">
-                &gt;
-            </button>
-        </div>
-    </div>
+			<div class="row owl-carousel wow fadeInUp mt-5 carousels" id="doctorSlideshow">
+				<div class="item col">
+					<div class="card-doctor">
+						<div class="header">
+							<img class="owl-img" src="../assets/img/10 slope.jpg" alt="">
+						</div>
+					</div>
+				</div>
+				<div class="item col">
+					<div class="card-doctor">
+						<div class="header">
+							<img class="owl-img" src="../assets/img/13 slope.jpg" alt="">
+						</div>
+					</div>
+				</div>
+				<div class="item col">
+					<div class="card-doctor">
+						<div class="header">
+							<img class="owl-img" src="../assets/img/17 slope.jpg" alt="">
+						</div>
+					</div>
+				</div>
+				<div class="item col">
+					<div class="card-doctor">
+						<div class="header">
+							<img class="owl-img" src="../assets/img/21 slope.jpg" alt="">
+						</div>
+					</div>
+				</div>
+				<div class="item col">
+					<div class="card-doctor">
+						<div class="header">
+							<img class="owl-img" src="../assets/img/23 slope.jpg" alt="">
+						</div>
+					</div>
+				</div>
+				<div class="item col">
+					<div class="card-doctor">
+						<div class="header">
+							<img class="owl-img" src="../assets/img/17 slope.jpg" alt="">
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
 </x-base-layout>
