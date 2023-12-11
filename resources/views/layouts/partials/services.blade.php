ef468aab01c5e430d0b1b0cef3a8be9c09a8bf77
robmainah <robmainah@gmail.com>
2023-12-11 12:18:34 +0300

    small fixes

--- resources/views/layouts/partials/services.blade.php
+++ resources/views/layouts/partials/services.blade.php
@@ -1,63 +1,63 @@
 <div class="services pb-5">
-    <div class="container">
-        <h2 class="text-center wow fadeInUp h1 title">Our services</h2>
+	<div class="container">
+		<h2 class="text-center wow fadeInUp h1 title">Our services</h2>
 
-        <div class="row owl-carousel wow fadeInUp" id="doctorSlideshow">
-            <div class="item col">
-                <div class="card-doctor">
-                    <div class="header">
-                        <img class="owl-img" src="../assets/img/17 slope.jpg" alt="">
-                    </div>
-                    <div class="body">
-                        <p class="text-xl mb-0">Zip Lining</p>
-                        <span class="text-sm text-grey"></span>
-                    </div>
-                </div>
-            </div>
-            <div class="item col">
-                <div class="card-doctor">
-                    <div class="header">
-                        <img class="owl-img" src="../assets/img/14 slope.jpg" alt="">
-                    </div>
-                    <div class="body">
-                        <p class="text-xl mb-0">Romantic Cottages</p>
-                        <span class="text-sm text-grey"></span>
-                    </div>
-                </div>
-            </div>
-            <div class="item col">
-                <div class="card-doctor">
-                    <div class="header">
-                        <img class="owl-img" src="../assets/img/10 slope.jpg" alt="">
-                    </div>
-                    <div class="body">
-                        <p class="text-xl mb-0">Pools</p>
-                        <span class="text-sm text-grey"></span>
-                    </div>
-                </div>
-            </div>
-            <div class="item col">
-                <div class="card-doctor">
-                    <div class="header">
-                        <img class="owl-img" src="../assets/img/21 slope.jpg" alt="">
-                    </div>
-                    <div class="body">
-                        <p class="text-xl mb-0"></p>
-                        <span class="text-sm text-grey"></span>
-                    </div>
-                </div>
-            </div>
-            <div class="item col">
-                <div class="card-doctor">
-                    <div class="header">
-                        <img class="owl-img" src="../assets/img/13 slope.jpg" alt="">
-                    </div>
-                    <div class="body">
-                        <p class="text-xl mb-0">Nature </p>
-                        <span class="text-sm text-grey"></span>
-                    </div>
-                </div>
-            </div>
-        </div>
-    </div>
+		<div class="row owl-carousel wow fadeInUp" id="doctorSlideshow">
+			<div class="item col">
+				<div class="card-doctor">
+					<div class="header">
+						<img class="owl-img" src="../assets/img/17 slope.jpg" alt="">
+					</div>
+					<div class="body">
+						<p class="text-xl mb-0">Zip Lining</p>
+						<span class="text-sm text-grey"></span>
+					</div>
+				</div>
+			</div>
+			<div class="item col">
+				<div class="card-doctor">
+					<div class="header">
+						<img class="owl-img" src="../assets/img/14 slope.jpg" alt="">
+					</div>
+					<div class="body">
+						<p class="text-xl mb-0">Romantic Cottages</p>
+						<span class="text-sm text-grey"></span>
+					</div>
+				</div>
+			</div>
+			<div class="item col">
+				<div class="card-doctor">
+					<div class="header">
+						<img class="owl-img" src="../assets/img/10 slope.jpg" alt="">
+					</div>
+					<div class="body">
+						<p class="text-xl mb-0">Pools</p>
+						<span class="text-sm text-grey"></span>
+					</div>
+				</div>
+			</div>
+			<div class="item col">
+				<div class="card-doctor">
+					<div class="header">
+						<img class="owl-img" src="../assets/img/21 slope.jpg" alt="">
+					</div>
+					<div class="body">
+						<p class="text-xl mb-0"></p>
+						<span class="text-sm text-grey"></span>
+					</div>
+				</div>
+			</div>
+			<div class="item col">
+				<div class="card-doctor">
+					<div class="header">
+						<img class="owl-img" src="../assets/img/13 slope.jpg" alt="">
+					</div>
+					<div class="body">
+						<p class="text-xl mb-0">Nature </p>
+						<span class="text-sm text-grey"></span>
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
 </div>
