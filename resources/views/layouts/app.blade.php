6a08974153caa0be1d29f231e4996cd304703219
robmainah <robmainah@gmail.com>
2023-12-11 15:12:19 +0300

    create admin reusable template

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -1,22 +1,21 @@
 <!DOCTYPE html>
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+
     <head>
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
         <meta name="csrf-token" content="{{ csrf_token() }}">
 
-        <title>{{ config('app.name', 'Laravel') }}</title>
-
-        <!-- Fonts -->
-        <link rel="preconnect" href="https://fonts.bunny.net">
-        <link href="https://fonts.bunny.net/css?family=figtree:400,500,600&display=swap" rel="stylesheet" />
+        <title>ZionValley</title>
 
         <!-- Scripts -->
         @vite(['resources/css/app.css', 'resources/js/app.js'])
 
         <!-- Styles -->
-        @livewireStyles
+
+        @include('layouts.admin.partials.links')
     </head>
+
     <body class="font-sans antialiased">
         <x-banner />
 
@@ -24,13 +23,13 @@
             @livewire('navigation-menu')
 
             <!-- Page Heading -->
-            @if (isset($header))
-                <header class="bg-white shadow">
-                    <div class="max-w-7xl mx-auto py-6 px-4 sm:px-6 lg:px-8">
-                        {{ $header }}
-                    </div>
-                </header>
-            @endif
+            <header class="shadow">
+                <div class="max-w-7xl mx-auto py-6 px-4 sm:px-6 lg:px-8">
+                    {{ $header }}
+                    {{-- Navs --}}
+                    @include('layouts.admin.partials.headers')
+                </div>
+            </header>
 
             <!-- Page Content -->
             <main>
@@ -41,5 +40,7 @@
         @stack('modals')
 
         @livewireScripts
+        @include('layouts.admin.partials.scripts')
     </body>
+
 </html>
