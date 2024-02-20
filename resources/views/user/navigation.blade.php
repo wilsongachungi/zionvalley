5e10925995fb25d312954373ed93ea5ef88bb712
wilsongachungi <gachungiwilson91@gmail.com>
2024-02-20 09:07:01 +0300

    changig chatting system

--- resources/views/user/navigation.blade.php
+++ resources/views/user/navigation.blade.php
@@ -57,14 +57,14 @@
                 <span class="menu-title">Add Tasks</span>
             </a>
         </li>
-        <li class="nav-item menu-items">
+        {{-- <li class="nav-item menu-items">
             <a class="nav-link" href="{{ route('see_list') }}">
                 <span class="menu-icon">
                     <i class="mdi mdi-contacts"></i>
                 </span>
                 <span class="menu-title">See Tasks</span>
             </a>
-        </li>
+        </li> --}}
         <li class="nav-item menu-items">
             <a class="nav-link" href="{{ route('credit_harambee') }}">
                 <span class="menu-icon">
