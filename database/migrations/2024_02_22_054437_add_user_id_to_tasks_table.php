7a536415aacd65e6150b823034ee187bf609c827
robmainah <robmainah@gmail.com>
2024-02-23 13:48:50 +0300

    Revert "comment adding column"
    This reverts commit e61ba8384fe2e96604c2118d5dc2c9ad275807e0.

--- database/migrations/2024_02_22_054437_add_user_id_to_tasks_table.php
+++ database/migrations/2024_02_22_054437_add_user_id_to_tasks_table.php
@@ -4,14 +4,15 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-return new class extends Migration {
+return new class extends Migration
+{
 	/**
 	 * Run the migrations.
 	 */
 	public function up(): void
 	{
 		Schema::table('tasks', function (Blueprint $table) {
-			// $table->foreignId('user_id')->constrained()->onDelete('cascade');
+			$table->foreignId('user_id')->constrained()->onDelete('cascade');
 		});
 	}
 
