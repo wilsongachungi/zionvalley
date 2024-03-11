713ad97754ce9164dda2cf14163f3bb38c0d434e
Willie <118902717+wilsongachungi@users.noreply.github.com>
2024-03-11 15:30:44 +0300

    changes

--- resources/views/pages/project.blade.php
+++ resources/views/pages/project.blade.php
@@ -1,5 +1,11 @@
 @section('css')
 <style>
+.carousel-item img {
+    height: 300px;
+    /* Adjust the height as needed */
+    object-fit: cover;
+}
+
 p {
     color: antiquewhite;
 }
@@ -88,101 +94,104 @@ p {
             </ul>
         </div>
         <div class="container">
-        <div class="center">
-            <h4>Ongoing Project</h4>
-        </div>
-        <div id="carouselOngoingProject" class="carousel slide" data-ride="carousel" data-interval="false">
-            <ol class="carousel-indicators">
-                <li data-target="#carouselOngoingProject" data-slide-to="0" class="active"></li>
-                <li data-target="#carouselOngoingProject" data-slide-to="1"></li>
-                <li data-target="#carouselOngoingProject" data-slide-to="2"></li>
-            </ol>
-            <div class="carousel-inner">
-                <div class="carousel-item active">
-                    <img class="d-block w-100" src="../assets/img/Cottage Rendered 1.jpg" alt="First slide">
-                </div>
-                <div class="carousel-item">
-                    <img class="d-block w-100" src="../assets/img/Capture11.JPG" alt="Second slide">
-                </div>
-                <div class="carousel-item">
-                    <img class="d-block w-100" src="../assets/img/Capture11.JPG" alt="Third slide">
-                </div>
+            <div class="center">
+                <h4>Ongoing Project</h4>
             </div>
-            <a class="carousel-control-prev" href="#carouselOngoingProject" role="button" data-slide="prev">
-                <span class="carousel-control-prev-icon" aria-hidden="true"></span>
-                <span class="sr-only">Previous</span>
-            </a>
-            <a class="carousel-control-next" href="#carouselOngoingProject" role="button" data-slide="next">
-                <span class="carousel-control-next-icon" aria-hidden="true"></span>
-                <span class="sr-only">Next</span>
-            </a>
-        </div>
-
-        <div class="center">
-            <h4>Future Projects</h4>
-        </div>
-
-        <div id="carouselOngoingProject" class="carousel slide" data-ride="carousel" data-interval="false">
-            <ol class="carousel-indicators">
-                <li data-target="#carouselFutureProjects" data-slide-to="0" class="active"></li>
-                <li data-target="#carouselFutureProjects" data-slide-to="1"></li>
-                <li data-target="#carouselFutureProjects" data-slide-to="2"></li>
-            </ol>
-            <div class="carousel-inner">
-                <div class="carousel-item active">
-                    <img class="d-block w-100" src="../assets/img/HandRail Tree Branches 02.JPG" alt="First slide">
+            <div id="carouselOngoingProject" class="carousel slide" data-ride="carousel" data-interval="false">
+                <ol class="carousel-indicators">
+                    <li data-target="#carouselOngoingProject" data-slide-to="0" class="active"></li>
+                    <li data-target="#carouselOngoingProject" data-slide-to="1"></li>
+                    <li data-target="#carouselOngoingProject" data-slide-to="2"></li>
+                </ol>
+                <div class="carousel-inner">
+                    <div class="carousel-item active">
+                        <img class="d-block w-100 h-100" src="../assets/img/Cottage Rendered 1.jpg" alt="First slide">
+                    </div>
+                    <div class="carousel-item">
+                        <img class="d-block w-100 h-100 " src="../assets/img/Capture11.JPG" alt="Second slide">
+                    </div>
+                    <div class="carousel-item">
+                        <img class="d-block w-100 h-100" src="../assets/img/Capture11.JPG" alt="Third slide">
+                    </div>
                 </div>
-                <div class="carousel-item">
-                    <img class="d-block w-100" src="../assets/img/forest-environment-with-shipping-container-set-int.jpg" alt="Second slide">public/assets/img/forest-environment-with-shipping-container-set-int.jpg
-                </div>
-                <div class="carousel-item">
-                    <img class="d-block w-100" src="../assets/img/14 slope.jpg" alt="Third slide">
-                </div>
+                <a class="carousel-control-prev" href="#carouselOngoingProject" role="button" data-slide="prev">
+                    <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+                    <span class="sr-only">Previous</span>
+                </a>
+                <a class="carousel-control-next" href="#carouselOngoingProject" role="button" data-slide="next">
+                    <span class="carousel-control-next-icon" aria-hidden="true"></span>
+                    <span class="sr-only">Next</span>
+                </a>
             </div>
-            <a class="carousel-control-prev" href="#carouselFutureProjects" role="button" data-slide="prev">
-                <span class="carousel-control-prev-icon" aria-hidden="true"></span>
-                <span class="sr-only">Previous</span>
-            </a>
-            <a class="carousel-control-next" href="#carouselFutureProjects" role="button" data-slide="next">
-                <span class="carousel-control-next-icon" aria-hidden="true"></span>
-                <span class="sr-only">Next</span>
-            </a>
-        </div>
 
-        <div class="center">
-            <h4>Inspirational Projects</h4>
-        </div>
-
-        <div id="carouselOngoingProject" class="carousel slide" data-ride="carousel" data-interval="false">
-            <ol class="carousel-indicators">
-                <li data-target="#carouselInspirationalProjects" data-slide-to="0" class="active"></li>
-                <li data-target="#carouselInspirationalProjects" data-slide-to="1"></li>
-                <li data-target="#carouselInspirationalProjects" data-slide-to="2"></li>
-            </ol>
-            <div class="carousel-inner">
-                <div class="carousel-item active">
-                    <img class="d-block w-100" src="../assets/img/13 slope.jpg" alt="First slide">
-                </div>
-                <div class="carousel-item">
-                    <img class="d-block w-100" src="../assets/img/17 slope.jpg" alt="Second slide">
-                </div>
-                <div class="carousel-item">
-                    <img class="d-block w-100" src="../assets/img/21 slope.jpg" alt="Third slide">
-                </div>
+            <div class="center">
+                <h4>Future Projects</h4>
             </div>
-            <a class="carousel-control-prev" href="#carouselInspirationalProjects" role="button" data-slide="prev">
-                <span class="carousel-control-prev-icon" aria-hidden="true"></span>
-                <span class="sr-only">Previous</span>
-            </a>
-            <a class="carousel-control-next" href="#carouselInspirationalProjects" role="button" data-slide="next">
-                <span class="carousel-control-next-icon" aria-hidden="true"></span>
-                <span class="sr-only">Next</span>
-            </a>
-        </div>
-    </div>
 
-    <!-- Bootstrap JS -->
-    <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js"></script>
-    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.bundle.min.js"></script>
+            <div id="carouselFutureProjects" class="carousel slide" data-ride="carousel" data-interval="false">
+                <ol class="carousel-indicators">
+                    <li data-target="#carouselFutureProjects" data-slide-to="0" class="active"></li>
+                    <li data-target="#carouselFutureProjects" data-slide-to="1"></li>
+                    <li data-target="#carouselFutureProjects" data-slide-to="2"></li>
+                </ol>
+                <div class="carousel-inner">
+                    <div class="carousel-item active">
+                        <img class="d-block w-100 h-100" src="../assets/img/HandRail Tree Branches 02.JPG"
+                            alt="First slide">
+                    </div>
+                    <div class="carousel-item">
+                        <img class="d-block w-100 h-100"
+                            src="../assets/img/forest-environment-with-shipping-container-set-int.jpg"
+                            alt="Second slide">
+                    </div>
+                    <div class="carousel-item">
+                        <img class="d-block w-100 h-100" src="../assets/img/14 slope.jpg" alt="Third slide">
+                    </div>
+                </div>
+                <a class="carousel-control-prev" href="#carouselFutureProjects" role="button" data-slide="prev">
+                    <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+                    <span class="sr-only">Previous</span>
+                </a>
+                <a class="carousel-control-next" href="#carouselFutureProjects" role="button" data-slide="next">
+                    <span class="carousel-control-next-icon" aria-hidden="true"></span>
+                    <span class="sr-only">Next</span>
+                </a>
+            </div>
+
+            <div class="center">
+                <h4>Inspirational Projects</h4>
+            </div>
+
+            <div id="carouselInspirationalProjects" class="carousel slide" data-ride="carousel" data-interval="false">
+                <ol class="carousel-indicators">
+                    <li data-target="#carouselInspirationalProjects" data-slide-to="0" class="active"></li>
+                    <li data-target="#carouselInspirationalProjects" data-slide-to="1"></li>
+                    <li data-target="#carouselInspirationalProjects" data-slide-to="2"></li>
+                </ol>
+                <div class="carousel-inner">
+                    <div class="carousel-item active">
+                        <img class="d-block w-100 h-100" src="../assets/img/13 slope.jpg" alt="First slide">
+                    </div>
+                    <div class="carousel-item">
+                        <img class="d-block w-100 h-100" src="../assets/img/17 slope.jpg" alt="Second slide">
+                    </div>
+                    <div class="carousel-item">
+                        <img class="d-block w-100 h-100" src="../assets/img/21 slope.jpg" alt="Third slide">
+                    </div>
+                </div>
+                <a class="carousel-control-prev" href="#carouselInspirationalProjects" role="button" data-slide="prev">
+                    <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+                    <span class="sr-only">Previous</span>
+                </a>
+                <a class="carousel-control-next" href="#carouselInspirationalProjects" role="button" data-slide="next">
+                    <span class="carousel-control-next-icon" aria-hidden="true"></span>
+                    <span class="sr-only">Next</span>
+                </a>
+            </div>
+        </div>
+
+        <!-- Bootstrap JS -->
+        <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js"></script>
+        <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.bundle.min.js"></script>
 
 </x-base-layout>
