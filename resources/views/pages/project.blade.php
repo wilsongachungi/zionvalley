bc4a4fd98c9cb5d1cc7e77143d0a5193b655b50d
Willie <118902717+wilsongachungi@users.noreply.github.com>
2024-03-19 12:12:13 +0300

    change

--- resources/views/pages/project.blade.php
+++ resources/views/pages/project.blade.php
@@ -370,7 +370,7 @@ p {
                             <button class="btn btn-link collapsed" data-toggle="collapse" data-target="#collapseThree"
                                 aria-expanded="false" aria-controls="collapseThree"
                                 style="color: greenyellow; text-decoration: none;">
-                                Nature Walks
+                                Guarden Furniture
                             </button>
                         </h5>
                     </div>
@@ -382,14 +382,29 @@ p {
                                 <!-- Carousel content -->
                                 <div class="carousel-inner">
                                     <div class="carousel-item active">
-                                        <img class="d-block w-100" src="../assets/img/13 slope.jpg" alt="First slide">
+                                        <img class="d-block w-100" src="../assets/img/1.jpg" alt="First slide">
                                     </div>
                                     <div class="carousel-item">
-                                        <img class="d-block w-100" src="../assets/img/17 slope.jpg" alt="Second slide">
+                                        <img class="d-block w-100" src="../assets/img/5.jpg" alt="Second slide">
                                     </div>
                                     <div class="carousel-item">
-                                        <img class="d-block w-100" src="../assets/img/21 slope.jpg" alt="Third slide">
+                                        <img class="d-block w-100" src="../assets/img/6.jpg" alt="Third slide">
                                     </div>
+                                    <div class="carousel-item">
+                                        <img class="d-block w-100" src="../assets/img/6a.jpg" alt="Third slide">
+                                    </div>
+                                    <div class="carousel-item">
+                                        <img class="d-block w-100" src="../assets/img/11.jpg" alt="Third slide">
+                                    </div>
+                                    <div class="carousel-item">
+                                        <img class="d-block w-100" src="../assets/img/21.jpg" alt="Third slide">
+                                    </div><div class="carousel-item">
+                                        <img class="d-block w-100" src="../assets/img/21b.jpg" alt="Third slide">
+                                    </div>
+                                    <div class="carousel-item">
+                                        <img class="d-block w-100" src="../assets/img/Mugumo Hollow Trunck.jpg" alt="Third slide">
+                                    </div>
+
                                 </div>
                                 <!-- Carousel navigation controls -->
                                 <a class="carousel-control-prev" href="#carouselInspirationalProjects3" role="button"
