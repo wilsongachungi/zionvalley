33d19df515ae843ffeeff195b9d1a6056ddfb7bc
Willie <118902717+wilsongachungi@users.noreply.github.com>
2024-03-22 12:02:10 +0300

    change

--- resources/views/pages/index.blade.php
+++ resources/views/pages/index.blade.php
@@ -101,7 +101,7 @@ li {
                             Activity center, and Eco resort.
                         </p>
                     </div>
-                    <img class="img1 pb-0 mb-0" src="../assets/img/home 1.jpg" alt="Image 1">
+                    <img class="img1 pb-0 mb-0" src="../assets/img/Home 1.jpg" alt="Image 1">
                 </div>
 
                 <div class="carousel-item text-center">
