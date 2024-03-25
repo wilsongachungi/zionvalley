1f9c7ca74067d35f084e56651f4e2f800509145c
Willie <118902717+wilsongachungi@users.noreply.github.com>
2024-03-25 14:05:44 +0300

    Update headers.blade.php

--- resources/views/layouts/user/partials/headers.blade.php
+++ resources/views/layouts/user/partials/headers.blade.php
@@ -1,38 +1,22 @@
 <header>
-<style>
+    <style>
     /* CSS styles for the navigation items */
     .navbar-nav .nav-item a.nav-link {
-      color: antiquewhite; /* Set default text color to white */
+        color: antiquewhite;
+        /* Set default text color to white */
     }
 
     .navbar-nav .nav-item.active a.nav-link,
     .navbar-nav .nav-item a.nav-link:hover {
-      color: greenyellow; /* Set background color on hover and active */
+        color: greenyellow;
+        /* Set background color on hover and active */
     }
+
     .navbar {
-      background-color: rgba(0, 0, 0, 0.5); /* Black with 70% transparency */
+        background-color: rgba(0, 0, 0, 0.5);
+        /* Black with 70% transparency */
     }
-  </style>
-    {{-- <div class="topbar">
-        <div class="container">
-            <div class="row">
-                <div class="col-sm-8 text-sm">
-                    <div class="site-info">
-                        <a href="#"><span class="mai-call text-primary"></span> +254 792 668219</a>
-                        <span class="divider">|</span>
-                        <a href="#"><span class="mai-mail text-primary"></span>zionvalleykenya@gmail.com</a>
-                    </div>
-                </div>
-                <div class="col-sm-4 text-right text-sm">
-                    <div class="social-mini-button">
-                        <a href="#"><span class="mai-logo-facebook-f"></span></a>
-                        <a href="#"><span class="mai-logo-whatsapp"></span></a>
-                        <a href="#"><span class="mai-logo-instagram"></span></a>
-                    </div>
-                </div>
-            </div>
-        </div>
-    </div> --}}
+    </style>
 
     <nav class="navbar navbar-expand-lg navbar-light shadow-sm fixed-top">
         <div class="container">
@@ -55,78 +39,57 @@
                         <a class="nav-link" href="{{ route('project') }}">Projects</a>
                     </li>
                     <li class="nav-item {{ Request::is('accomodation') ? 'active' : '' }}">
-                        <a class="nav-link" href="{{ route('accomodation') }}">Accomodation</a>
+                        <a class="nav-link" href="{{ route('accomodation') }}">Accommodation</a>
                     </li>
                     <li class="nav-item {{ Request::is('harambee') ? 'active' : '' }}">
                         <a class="nav-link" href="{{ route('harambee') }}">Harambee</a>
                     </li>
                     <li class="nav-item {{ Request::is('contactzion') ? 'active' : '' }}">
                         <a class="nav-link" href="{{ route('contactzion') }}">Connect</a>
-
-                        @guest
-
-                    <li class="nav-item {{ Request::is('login') ? 'active' : '' }}">
-                        <a style="color:green" class="nav-link" href="{{ route('login') }}">Login</a>
                     </li>
-                    @endguest
-                    </li>
+
+
+
 
                     <li class="nav-item {{ Request::is('team') ? 'active' : '' }}">
                         <a class="nav-link" href="{{ route('team') }}">Team</a>
                     </li>
-                    <li>
-                        @if (Route::has('login'))
-                        @auth
-
-                        {{-- <li class="nav-item {{ Request::is('') ? 'active' : '' }}">
-                        <a style="color:green" class="nav-link" href="{{ route('message') }}">Profile</a>
-                    </li> --}}
-                    @auth
-                    @php
-                    $fullName = Auth::user()->name;
-                    $firstName = explode(' ', $fullName)[0];
-                    @endphp
-
-                    <div 
-                        style="position: absolute; top: 3px; right: 2px; padding: 8px; background-color: display: flex; align-items: center;">
-                        <div class="dropdown">
-                            <button type="button" class="btn btn-secondary dropdown-toggle" data-toggle="dropdown"
-                                aria-haspopup="true" aria-expanded="false">
-                                {{ $firstName }}
-                            </button>
-                            <!-- <div class="dropdown-menu">
-                                <form method="POST" action="{{ route('logout') }}">
-                                    @csrf
-                                    <button type="submit" class="dropdown-item">Logout</button>
-                                </form>
-                                <a href="{{ route('info') }}" class="dropdown-item">Profile</a>
-                                <a href="{{ route('profile.show') }}" class="dropdown-item">Change Password</a>
-                            </div> -->
-                        </div>
-                    </div>
-                    @endauth
-                    @else
-                    {{-- <li class="nav-item {{ Request::is('') ? 'active' : '' }}">
-                    <a style="color:green" class="nav-link" href="{{ route('login') }}">login</a>
-                    </li> --}}
-                    @endauth
-                    @endif
-                    </li>
-
                 </ul>
+
             </div>
         </div>
-    </nav>
-    <!-- <div class="dropdown">
-        <button class="btn btn-secondary dropdown-toggle" type="button" id="dropdownMenuButton" data-toggle="dropdown"
-            aria-haspopup="true" aria-expanded="false">
-            Dropdown button
-        </button>
-        <div class="dropdown-menu" aria-labelledby="dropdownMenuButton">
-            <a class="dropdown-item" href="#">Action</a>
-            <a class="dropdown-item" href="#">Another action</a>
-            <a class="dropdown-item" href="#">Something else here</a>
-        </div>
-    </div> -->
+        @auth
+        @php
+        $fullName = Auth::user()->name;
+        $firstName = explode(' ', $fullName)[0];
+        @endphp
 
+    </nav>
+    <div class="centered-container" style="margin-top:50px; position:absolute;z-index:1000; align-item:center;  justify-content: center; right:20px; margin-top:70px">
+        @auth
+        <div class="dropdown">
+            <button class="btn btn-secondary dropdown-toggle" type="button" id="userDropdownMenu" data-toggle="dropdown"
+                aria-haspopup="true" aria-expanded="false">
+                {{ $firstName }}
+            </button>
+            <div class="dropdown-menu" aria-labelledby="userDropdownMenu">
+                <form method="POST" action="{{ route('logout') }}">
+                    @csrf
+                    <button type="submit" class="dropdown-item">Logout</button>
+                </form>
+                <a href="{{ route('info') }}" class="dropdown-item">Profile</a>
+                <a href="{{ route('profile.show') }}" class="dropdown-item">Change Password</a>
+            </div>
+        </div>
+        @endauth
+
+        @else
+        {{-- Show login link if not logged in --}}
+        <li class="nav-item {{ Request::is('login') ? 'active' : '' }}" style="list-style-type: none;">
+            <a style="color:green" class="nav-link" href="{{ route('login') }}">Login</a>
+        </li>
+
+
+        @endauth
+    </div>
 </header>
