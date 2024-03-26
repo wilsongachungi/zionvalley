733ac76fa7ca372cd7c97fcae2d05d312973a1fc
Willie <118902717+wilsongachungi@users.noreply.github.com>
2024-03-26 10:18:17 +0300

    debugging

--- resources/views/layouts/user/partials/headers.blade.php
+++ resources/views/layouts/user/partials/headers.blade.php
@@ -1,20 +1,33 @@
 <header>
+
+    <head>
+        <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
+        <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.bundle.min.js"></script>
+
+    </head>
     <style>
-    /* CSS styles for the navigation items */
     .navbar-nav .nav-item a.nav-link {
         color: antiquewhite;
-        /* Set default text color to white */
+
     }
 
     .navbar-nav .nav-item.active a.nav-link,
     .navbar-nav .nav-item a.nav-link:hover {
         color: greenyellow;
-        /* Set background color on hover and active */
+
     }
 
     .navbar {
         background-color: rgba(0, 0, 0, 0.5);
-        /* Black with 70% transparency */
+
+    }
+
+    .btn-secondary {
+        background-color: transparent !important;
+        color: inherit;
+        /* Optional: Inherit text color from parent */
+        border-color: inherit;
+        /* Optional: Inherit border color from parent */
     }
     </style>
 
@@ -24,72 +37,74 @@
 
             <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupport"
                 aria-controls="navbarSupport" aria-expanded="false" aria-label="Toggle navigation">
-                <span style="background-color:rgba(0,78,0)" class="navbar-toggler-icon"></span>
+                <span style="background-color: rgba(0, 78, 0)" class="navbar-toggler-icon"></span>
             </button>
+
+
+
 
             <div class="collapse navbar-collapse" id="navbarSupport">
                 <ul class="navbar-nav ml-auto">
-                    <li class="nav-item {{ Request::route()->getName() === 'index' ? 'active' : '' }}">
+      
+                    <li  style="list-style-type: none;" class="nav-item {{ Request::route()->getName() === 'index' ? 'active' : '' }}">
                         <a class="nav-link" href="{{ route('index') }}">Welcome to Zion</a>
                     </li>
-                    <li class="nav-item {{ Request::is('community') ? 'active' : '' }}">
-                        <a class="nav-link" href="{{ route('community') }}">Community Environment</a>
+                    <li  style="list-style-type: none;" class="nav-item {{ Request::is('community') ? 'active' : '' }}">
+                        <a class="nav-link" href="{{ route('community') }}">Community</a>
                     </li>
-                    <li class="nav-item {{ Request::is('project') ? 'active' : '' }}">
+                    <li  style="list-style-type: none;" class="nav-item {{ Request::is('project') ? 'active' : '' }}">
                         <a class="nav-link" href="{{ route('project') }}">Projects</a>
                     </li>
-                    <li class="nav-item {{ Request::is('accomodation') ? 'active' : '' }}">
+                    <li  style="list-style-type: none;" class="nav-item {{ Request::is('accomodation') ? 'active' : '' }}">
                         <a class="nav-link" href="{{ route('accomodation') }}">Accommodation</a>
                     </li>
-                    <li class="nav-item {{ Request::is('harambee') ? 'active' : '' }}">
+                    <li  style="list-style-type: none;" class="nav-item {{ Request::is('harambee') ? 'active' : '' }}">
                         <a class="nav-link" href="{{ route('harambee') }}">Harambee</a>
                     </li>
-                    <li class="nav-item {{ Request::is('contactzion') ? 'active' : '' }}">
+                    <li  style="list-style-type: none;" class="nav-item {{ Request::is('contactzion') ? 'active' : '' }}">
                         <a class="nav-link" href="{{ route('contactzion') }}">Connect</a>
                     </li>
 
-
-
-
-                    <li class="nav-item {{ Request::is('team') ? 'active' : '' }}">
+                   <li  style="list-style-type: none;" class="nav-item {{ Request::is('team') ? 'active' : '' }}">
                         <a class="nav-link" href="{{ route('team') }}">Team</a>
                     </li>
                 </ul>
-
-            </div>
+           </div>
         </div>
         @auth
-        @php
-        $fullName = Auth::user()->name;
-        $firstName = explode(' ', $fullName)[0];
-        @endphp
+                    @php
+                    $fullName = Auth::user()->name;
+                    $firstName = explode(' ', $fullName)[0];
+                    @endphp
+                    <div class="centered-container"
+                        style="margin-top:50px; position:absolute;z-index:1000; align-item:center;  justify-content: center; right:20px; margin-top:70px">
+                        @auth
+                        <div class="dropdown">
+                            <button class="btn btn-secondary dropdown-toggle" type="button" id="userDropdownMenu"
+                                data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" style="color:green">
+                                {{ $firstName }}
+                            </button>
+                            <div class="dropdown-menu" aria-labelledby="userDropdownMenu">
+                                <form method="POST" action="{{ route('logout') }}">
+                                    @csrf
+                                    <button type="submit" class="dropdown-item">Logout</button>
+                                </form>
+                                <a href="{{ route('info') }}" class="dropdown-item">Profile</a>
+                                <a href="{{ route('profile.show') }}" class="dropdown-item">Change Password</a>
+                            </div>
+                        </div>
+                        @endauth
 
+                        @else
+                        {{-- Show login link if not logged in --}}
+                        <li  style="list-style-type: none;" class="nav-item {{ Request::is('login') ? 'active' : '' }}" style="list-style-type: none;">
+                            <a style="color:green; " class="nav-link" href="{{ route('login') }}">Login</a>
+                        </li>
+
+
+                        @endauth
+                    </div>
     </nav>
-    <div class="centered-container" style="margin-top:50px; position:absolute;z-index:1000; align-item:center;  justify-content: center; right:20px; margin-top:70px">
-        @auth
-        <div class="dropdown">
-            <button class="btn btn-secondary dropdown-toggle" type="button" id="userDropdownMenu" data-toggle="dropdown"
-                aria-haspopup="true" aria-expanded="false">
-                {{ $firstName }}
-            </button>
-            <div class="dropdown-menu" aria-labelledby="userDropdownMenu">
-                <form method="POST" action="{{ route('logout') }}">
-                    @csrf
-                    <button type="submit" class="dropdown-item">Logout</button>
-                </form>
-                <a href="{{ route('info') }}" class="dropdown-item">Profile</a>
-                <a href="{{ route('profile.show') }}" class="dropdown-item">Change Password</a>
-            </div>
-        </div>
-        @endauth
-
-        @else
-        {{-- Show login link if not logged in --}}
-        <li class="nav-item {{ Request::is('login') ? 'active' : '' }}" style="list-style-type: none;">
-            <a style="color:green" class="nav-link" href="{{ route('login') }}">Login</a>
-        </li>
 
 
-        @endauth
-    </div>
 </header>
