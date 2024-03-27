695dfe95ccdd4016b46b03ff5147714584f14796
Willie <118902717+wilsongachungi@users.noreply.github.com>
2024-03-27 16:49:35 +0300

    resolving the migration

--- database/migrations/2024_03_25_061217_remove_avatar_from_users.php
+++ database/migrations/2024_03_25_061217_remove_avatar_from_users.php
@@ -9,21 +9,18 @@ return new class extends Migration
     /**
      * Run the migrations.
      */
-    public function up(): void
-    {
-        Schema::table('users', function (Blueprint $table) {
-            //
-        });
-    }
+    public function up()
+{
+    Schema::table('users', function (Blueprint $table) {
+        $table->string('avatar')->nullable();
+    });
+}
 
-    /**
-     * Reverse the migrations.
-     */
-    public function down(): void
-    {
-        Schema::table('users', function (Blueprint $table) {
-            $table->dropColumn('avatar');
-        });
-        
-    }
+public function down()
+{
+    Schema::table('users', function (Blueprint $table) {
+        $table->dropColumn('avatar');
+    });
+}
+
 };
