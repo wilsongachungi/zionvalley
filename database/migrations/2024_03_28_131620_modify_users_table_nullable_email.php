23f617f6b8139702ac476a604b1455b7c721d07a
Willie <118902717+wilsongachungi@users.noreply.github.com>
2024-03-28 18:05:21 +0300

    change

---
+++ database/migrations/2024_03_28_131620_modify_users_table_nullable_email.php
@@ -0,0 +1,29 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up()
+    {
+        Schema::table('users', function (Blueprint $table) {
+            $table->string('email')->unique()->nullable()->change();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down()
+    {
+        Schema::table('users', function (Blueprint $table) {
+            $table->dropUnique('users_email_unique'); // Drop the unique key constraint
+            $table->string('email')->change(); // Change the email column to its previous state
+        });
+    }
+};
