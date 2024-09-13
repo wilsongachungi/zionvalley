c3cc82b1217e4cb52377f98b510812b68ef37b49
wilsongachungi <gachungiwilson91@gmail.com>
2024-09-13 14:55:02 +0300

    show edit when there is something to edit

--- resources/views/layouts/admin.blade.php
+++ resources/views/layouts/admin.blade.php
@@ -15,8 +15,6 @@
     <link rel="stylesheet" href="admin/assets/css/style.css">
     <link rel="stylesheet" href="/assets/css/admin.css">
 
-    <link href="https://cdn.jsdelivr.net/npm/tailwindcss@2.2.19/dist/tailwind.min.css" rel="stylesheet">
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/css/bootstrap.min.css" rel="stylesheet">
 
     @yield('css')
 
