a5a84e366e8520b840184d0ee37c3e5994cbbf96
wilsongachungi <gachungiwilson91@gmail.com>
2024-09-21 10:11:09 +0300

    using checkbox
    How it works:
    $request->input('is_whatsapp', false): This tries to get the value of is_whatsapp from the request. If it’s not present (i.e., the checkbox is unchecked), it defaults to false.
    The ternary operator ? true : false is used to explicitly set $is_whatsapp and $is_mpesa as boolean values. If the checkbox was checked, the value will be true; otherwise, it will be false.

--- app/Http/Controllers/PhoneInfoController.php
+++ app/Http/Controllers/PhoneInfoController.php
@@ -12,26 +12,22 @@ class PhoneInfoController extends Controller
 	{
 		// Validate the incoming request
 		$request->validate([
-			'is_whatsapp' => 'nullable|boolean',
-			'is_mpesa' => 'nullable|boolean',
 			'category' => 'required|in:Financial Supporter,CBO Member,CBO Land Owner,Employee',
 		]);
 
 		// Handle checkboxes for is_whatsapp and is_mpesa
-		$is_whatsapp = $request->has('is_whatsapp') ? true : false;
-		$is_mpesa = $request->has('is_mpesa') ? true : false;
+		$is_whatsapp = $request->input('is_whatsapp', false) ? true : false;
+		$is_mpesa = $request->input('is_mpesa', false) ? true : false;
 
-		// Create a new instance of the PhoneInfo model
-		$phoneInfo = new PhoneInfo;
-
-		// Assign values to the model's attributes
-		$phoneInfo->user_id = Auth::id(); // Set the current authenticated user's ID
-		$phoneInfo->is_whatsapp = $is_whatsapp; // Assign the boolean value
-		$phoneInfo->is_mpesa = $is_mpesa; // Assign the boolean value
-		$phoneInfo->category = $request->input('category'); // Assign the selected category from the form
-
-		// Save the model to the database
-		$phoneInfo->save();
+		// Create or update the PhoneInfo model
+		PhoneInfo::updateOrCreate(
+			['user_id' => Auth::id()], // Use user ID as the unique identifier
+			[
+				'is_whatsapp' => $is_whatsapp,
+				'is_mpesa' => $is_mpesa,
+				'category' => $request->input('category'),
+			]
+		);
 
 		// Return a response indicating success
 		return redirect()->back()->with('status', 'Information saved successfully!');
