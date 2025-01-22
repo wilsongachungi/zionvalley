bd9af695acff544b3e5826b65159b2e149925313
wilsongachungi <gachungiwilson91@gmail.com>
2025-01-22 20:28:18 +0300

    testing

--- resources/views/pages/team.blade.php
+++ resources/views/pages/team.blade.php
@@ -36,7 +36,7 @@ li {
     <br>
     <h5 class="welcome-text">Team</h5>
     <section class="my-4">
-        <h4 class="text-center mb-3">Our Team</h3>
+        <h4 class="text-center mb-3">Our Team Team</h3>
             <div class="team-row">
                 <div class="team-col">
                     <div class="user d-flex flex-column col">
@@ -44,8 +44,8 @@ li {
                         <div class="user-info site-color text-center">Chairman</div>
                     </div>
                     <p class="px-2 line-height-1-5 text-center">
-                        {{-- Presiding over CBO meetings and working for the interests of the Zion Valley project,
-                        its land supporters, the Harambee owners and the local community. --}}
+                        Presiding over CBO meetings and working for the interests of the Zion Valley project,
+                        its land supporters, the Harambee owners and the local community.
                     </p>
                 </div>
                 <div class="team-col">
